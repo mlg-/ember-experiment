2dead9a1c721ffd01f025f31a6cec2cb16a2c063
Melissa Leigh Gore <melissaleighgore@gmail.com>
2016-02-02 15:32:05 -0500

    Fix massively wonky git setup

---
+++ frontend/app/models/book.js
@@ -0,0 +1,9 @@
+import DS from 'ember-data';
+
+var Book = DS.Model.extend({
+  title: DS.attr('string'),
+  author: DS.attr('string'),
+  description: DS.attr('string')
+});
+
+export default Book;
